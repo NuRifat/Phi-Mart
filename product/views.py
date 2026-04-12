7ed97a38494d8ae8b7b830b5ec0ad14f45ab61cd
NuRifat <najimullahrifat@gmail.com>
2026-04-13 01:02:06 +0600

    20.1 Api Naming Convention & uses of get_object_or_404

--- product/views.py
+++ product/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+#from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from product.models import Product, Category
 # Create your views here.
+
+
+@api_view()
+def view_specific_product(request, id):
+    product = get_object_or_404(Product, pk=id)
+    product_dict={'id':product.id, 'name':product.name, 'price':product.price}
+    return Response(product_dict)
+
+@api_view()
+def view_categories(request):
+    return Response({'message' : 'Categories'})

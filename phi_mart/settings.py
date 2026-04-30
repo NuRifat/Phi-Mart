ae6ce7c523a725c6bc0e6c7835227ad1841b04ee
NuRifat <najimullahrifat@gmail.com>
2026-05-01 02:29:22 +0600

    23.3 User Login & Fetching current user info

--- phi_mart/settings.py
+++ phi_mart/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,10 +130,12 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
+   "ACCESS_TOKEN_LIFETIME" : timedelta(days=1),
 }
 
 DJOSER = {
     'SERIALIZERS' : {
-        'user_create' : 'users.serializers.UserCreateSerializer'
+        'user_create' : 'users.serializers.UserCreateSerializer',
+        'current_user' : 'users.serializers.UserSerializer'
     },
 }

a9885964a2557e9b308dc9814c50bd2812231ecb
NuRifat <najimullahrifat@gmail.com>
2026-05-03 03:40:12 +0600

    23.7 Products Review update & securing review api

--- phi_mart/urls.py
+++ phi_mart/urls.py
@@ -7,8 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', api_root_view),
     #path("api-auth/", include("rest_framework.urls")), # for this logout option is available 
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
     path('api/v1/', include('api.urls'), name='api-root'),
 ] + debug_toolbar_urls()
 
--- product/models.py
+++ product/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
@@ -29,8 +31,14 @@ class Product(models.Model):
     
 class Review(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    description = models.TextField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    ratings = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], default=5)
+    comment = models.TextField()
     date = models.DateField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Review by {self.user.first_name} on {self.product.name}"
 
 # Step to build an API : Model, Serializer, ViewSet, Router

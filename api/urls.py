b32977f17de21a92fad720575821448bab0d6c13
NuRifat <najimullahrifat@gmail.com>
2026-07-27 12:16:55 +0600

    Check if an User ordered a product or not (Module-39.6)

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import SimpleRouter, DefaultRouter
 from product.views import ProductViewSet, CategoryViewSet, ReviewViewSet, ProductImageViewSet
 from rest_framework_nested import routers
-from order.views import CartViewSet, CartItemViewSet, OrderViewSet, initiate_payment, payment_success, payment_cancel, payment_fail
+from order.views import CartViewSet, CartItemViewSet, OrderViewSet, initiate_payment, payment_success, payment_cancel, payment_fail, HasOrderedProduct
 
 router = routers.DefaultRouter()
 router.register('products', ProductViewSet, basename='products')
@@ -27,6 +27,7 @@ urlpatterns = [
     path("payment/success/", payment_success, name="payment-success"),
     path("payment/fail/", payment_fail, name="payment-fail"),
     path("payment/cancel/", payment_cancel, name="payment-cancel"),
+    path("orders/has-ordered/<int:product_id>/", HasOrderedProduct.as_view()),
 ]
 
 # urlpatterns = [
